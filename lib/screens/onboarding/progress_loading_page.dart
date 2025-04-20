71ffcb3f515b3ae4cde02245f6116626315a9cfb
y <dkgk2233@gmail.com>
2025-04-20 13:22:18 +0900

    인풋 로딩페이지 변경

--- lib/screens/onboarding/progress_loading_page.dart
+++ lib/screens/onboarding/progress_loading_page.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:haengunse/service/manse/manse_interactor.dart';
 
 class ProgressLoadingPage extends StatefulWidget {
@@ -28,40 +29,41 @@ class _ProgressLoadingPageState extends State<ProgressLoadingPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Center(
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
+            padding: EdgeInsets.symmetric(horizontal: 10.w),
             child: Column(
-              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center, // 수직 가운데 정렬
+              crossAxisAlignment: CrossAxisAlignment.center, // 수평 가운데 정렬
+              mainAxisSize: MainAxisSize.max,
               children: [
-                SizedBox(height: 40),
                 SizedBox(
-                  width: 80,
-                  height: 80,
-                  child: CircularProgressIndicator(
+                  width: 80.w,
+                  height: 80.w,
+                  child: const CircularProgressIndicator(
                     color: Color(0xFF82c784),
                     strokeWidth: 6.5,
                   ),
                 ),
-                SizedBox(height: 40),
+                SizedBox(height: 40.h),
                 Text(
                   '내 정보 입력 완료',
                   style: TextStyle(
                     fontFamily: 'Pretendard',
-                    fontSize: 22,
+                    fontSize: 22.sp,
                     fontWeight: FontWeight.bold,
                     color: Colors.black87,
                   ),
                 ),
-                SizedBox(height: 5),
+                SizedBox(height: 5.h),
                 Text(
                   '작성하신 정보를 바탕으로\n운세를 계산하고 있어요',
                   style: TextStyle(
                     fontFamily: 'Pretendard',
-                    fontSize: 13,
+                    fontSize: 13.sp,
                     color: Colors.black54,
                     height: 1.5,
                   ),
