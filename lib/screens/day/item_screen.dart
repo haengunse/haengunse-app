f06cc396a05fb774d085415587eda181a13e5387
y <dkgk2233@gmail.com>
2025-04-26 23:26:17 +0900

    행운포인트 ui

--- lib/screens/day/item_screen.dart
+++ lib/screens/day/item_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ItemScreen extends StatelessWidget {
   final Map<String, dynamic> item;
@@ -7,19 +8,74 @@ class ItemScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("행운 아이템 화면")),
-      body: Padding(
-        padding: const EdgeInsets.all(24),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text("색상: ${item['color']}"),
-            Text("장소: ${item['place']}"),
-            Text("숫자: ${item['number']}"),
-            Text("사물: ${item['object']}"),
-          ],
-        ),
+    return Container(
+      width: 300.w,
+      height: 500.h,
+      padding: EdgeInsets.all(20.w),
+      decoration: BoxDecoration(
+        color: const Color(0xFFFFFEED),
+        borderRadius: BorderRadius.circular(20.r),
+      ),
+      child: Stack(
+        children: [
+          // 닫기 버튼
+          Positioned(
+            top: 0,
+            right: 0,
+            child: GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: const Icon(Icons.close, size: 24, color: Colors.black),
+            ),
+          ),
+          // 메인 콘텐츠
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 5.h),
+                Image.asset(
+                  'assets/images/item_card.png', // 🎁 아이템 카드 이미지
+                  width: 150.w,
+                  height: 150.w,
+                ),
+                SizedBox(height: 20.h),
+                Text(
+                  "행운세가 전하는",
+                  style: TextStyle(
+                    fontFamily: 'HakgyoansimDunggeunmiso',
+                    fontSize: 18.sp,
+                    fontWeight: FontWeight.w200,
+                    color: const Color.fromARGB(255, 96, 95, 95),
+                  ),
+                ),
+                Text(
+                  "오늘의 행운포인트",
+                  style: TextStyle(
+                    fontFamily: 'HakgyoansimDunggeunmiso',
+                    fontSize: 30.sp,
+                    fontWeight: FontWeight.bold,
+                    color: const Color.fromARGB(255, 96, 95, 95),
+                  ),
+                ),
+                SizedBox(height: 16.h),
+                Text(
+                  "색상: ${item['color']}\n"
+                  "장소: ${item['place']}\n"
+                  "숫자: ${item['number']}\n"
+                  "사물: ${item['object']}",
+                  style: TextStyle(
+                    fontFamily: 'HakgyoansimDunggeunmiso',
+                    fontSize: 22.sp,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    height: 1.5,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
