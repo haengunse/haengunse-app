f3c7481fbb8467f09767edebc1736124dc899afa
y <dkgk2233@gmail.com>
2025-04-26 23:18:37 +0900

    랜덤 질문 ui

--- lib/screens/day/random_screen.dart
+++ lib/screens/day/random_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class RandomScreen extends StatelessWidget {
   final String message;
@@ -7,9 +8,70 @@ class RandomScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("랜덤 질문 화면")),
-      body: Center(child: Text(message, style: const TextStyle(fontSize: 18))),
+    return Container(
+      width: 300.w,
+      height: 420.h,
+      padding: EdgeInsets.all(20.w),
+      decoration: BoxDecoration(
+        color: const Color(0xFFFFFEED),
+        borderRadius: BorderRadius.circular(20.r),
+      ),
+      child: Stack(
+        children: [
+          // 닫기 버튼
+          Positioned(
+            top: 0,
+            right: 0,
+            child: GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: const Icon(Icons.close, size: 24, color: Colors.black),
+            ),
+          ),
+          // 메인 콘텐츠
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 20.h),
+              Image.asset(
+                'assets/images/random_card.png',
+                width: 150.w,
+                height: 150.w,
+              ),
+              SizedBox(height: 20.h),
+              Text(
+                "행운세가 전하는",
+                style: TextStyle(
+                  fontFamily: 'HakgyoansimDunggeunmiso',
+                  fontSize: 18.sp,
+                  fontWeight: FontWeight.w200,
+                  color: const Color.fromARGB(255, 96, 95, 95),
+                ),
+              ),
+              Text(
+                "오늘의 질문",
+                style: TextStyle(
+                  fontFamily: 'HakgyoansimDunggeunmiso',
+                  fontSize: 30.sp,
+                  fontWeight: FontWeight.bold,
+                  color: const Color.fromARGB(255, 96, 95, 95),
+                ),
+              ),
+              SizedBox(height: 16.h),
+              Text(
+                "“$message”",
+                style: TextStyle(
+                  fontFamily: 'HakgyoansimDunggeunmiso',
+                  fontSize: 30.sp,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  height: 1.4,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
